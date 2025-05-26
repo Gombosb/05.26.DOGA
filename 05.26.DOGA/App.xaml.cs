6c6f3bf68caa5e729960758f823b5ed30e462972
gombosb <gombosb@301-08>
2025-05-26 07:52:00 +0200

    Add project files.

---
+++ 05.26.DOGA/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _05._26.DOGA
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
